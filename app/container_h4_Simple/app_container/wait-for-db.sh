c528759a44d6b831cf25dfe7a465b7d8bdaabfa0
Irene Gosálvez White <irenegwh@correo.ugr.es>
2025-01-22 17:27:17 +0100

    Render

--- app/container_h4_Simple/app_container/wait-for-db.sh
+++ app/container_h4_Simple/app_container/wait-for-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Espera a que PostgreSQL esté listo para aceptar conexiones
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h dpg-cu816b8gph6c7397vs6g-a -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
   echo "Esperando a que la base de datos esté lista..."
   sleep 2
 done
