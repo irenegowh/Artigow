aac344af97b2441632740de7f02397206827af26
Irene Gosálvez White <irenegwh@correo.ugr.es>
2025-01-22 17:32:53 +0100

    Render

--- app/container_h4_Simple/app_container/wait-for-db.sh
+++ app/container_h4_Simple/app_container/wait-for-db.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
 # Espera a que PostgreSQL esté listo para aceptar conexiones
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
+#until PGPASSWORD=$POSTGRES_PASSWORD psql -h db_service -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
+# Extraer los valores de la variable DATABASE_URL
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+# Si tienes la URL de la base de datos en la variable DATABASE_URL
+DB_URL=$DATABASE_URL
+
+# Extraer partes de la URL
+DB_HOST=$(echo $DB_URL | sed -e 's/.*\/\/\(.*\):.*/\1/')
+DB_PORT=$(echo $DB_URL | sed -e 's/.*:\([0-9]*\)\/.*/\1/')
+DB_USER=$(echo $DB_URL | sed -e 's/.*\/\/\(.*\):.*/\1/' | sed -e 's/:.*//')
+DB_NAME=$(echo $DB_URL | sed -e 's/.*\/\(.*\)/\1/')
+
+# Espera a que PostgreSQL esté listo para aceptar conexiones
+until psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -p "$DB_PORT" -c '\q'; do
   echo "Esperando a que la base de datos esté lista..."
   sleep 2
 done
