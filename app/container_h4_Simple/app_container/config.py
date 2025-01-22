56810ad08d6954be7c153e04dcea370dc7d5b099
Irene Gosálvez White <irenegwh@correo.ugr.es>
2025-01-22 16:40:27 +0100

    Render

--- app/container_h4_Simple/app_container/config.py
+++ app/container_h4_Simple/app_container/config.py
@@ -1,26 +1,30 @@
-#*************************#
-# Configuracion en local  #
-#*************************#
+import os
+
+# Configuración base
 class Config:
-    SECRET_KEY = 'xxxxyyyyyzzzzz'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@db_service:5432/artigow_db'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'xxxxyyyyyzzzzz')  # Puedes establecer SECRET_KEY como una variable de entorno
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'app/static/images'
 
+# Configuración para entorno local
+class LocalConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@db_service:5432/artigow_db'
+
+# Configuración para Render (Producción)
+class RenderConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Render proporciona DATABASE_URL automáticamente
+
+# Configuración para pruebas
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Para pruebas locales
-    TESTING = True  # Indicador para el entorno de pruebas
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    TESTING = True
 
-#*************************#
-# Configuracion en Render #
-#*************************#
-#class Config:
-#    SECRET_KEY = 'xxxxyyyyyzzzzz'
-#    # Usar la URL externa de la base de datos proporcionada por Render
-#    SQLALCHEMY_DATABASE_URI = 'postgresql://artigowdb_user:hOZs4UHZpYxNlCPtVX2Pr4kMCRA96X9M@dpg-cu816b8gph6c7397vs6g-a:5432/artigowdb'
-#    SQLALCHEMY_TRACK_MODIFICATIONS = False
-#    UPLOAD_FOLDER = 'app/static/images'
-
-#class TestConfig(Config):
-#    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Para pruebas locales
-#    TESTING = True  # Indicador para el entorno de pruebas
+# Seleccionar configuración según el entorno
+def get_config():
+    environment = os.getenv('FLASK_ENV', 'local')  # Por defecto, asumimos un entorno local
+    if environment == 'production':
+        return RenderConfig
+    elif environment == 'testing':
+        return TestConfig
+    else:
+        return LocalConfig
