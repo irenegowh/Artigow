1768ded66d6b4b19eeb0adcf0dbb375afb68d1b3
Irene Gosálvez White <irenegwh@correo.ugr.es>
2025-01-22 23:00:41 +0100

    Render

--- app/container_h4_Simple/app_container/app/run.py
+++ app/container_h4_Simple/app_container/app/run.py
@@ -10,14 +10,3 @@ with app.app_context():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
-try:
-    conn = psycopg2.connect(
-        dbname="artigowdb_2lqj",
-        user="user",
-        password="2ydBw1MGaHyvqbXYUiIWcweUsh5257SB",
-        host="dpg-cu8g8elsvqrc73baddcg-a.frankfurt-postgres.render.com",
-        port=5432
-    )
-    print("Connection successful!")
-except Exception as e:
-    print(f"Connection failed: {e}")
