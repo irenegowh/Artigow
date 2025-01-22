d51fb0edae3603435cbe9a18c1fa191dfecc5c24
Irene Gosálvez White <irenegwh@correo.ugr.es>
2025-01-22 17:48:18 +0100

    Render

--- app/container_h4_Simple/app_container/wait-for-db.sh
+++ app/container_h4_Simple/app_container/wait-for-db.sh
@@ -6,16 +6,7 @@
 #!/bin/sh
 
 # Extraer los valores de la variable DATABASE_URL
-export PGPASSWORD=$POSTGRES_PASSWORD  # Se usa para la conexión de psql
-
-# Obtener la URL de la base de datos de la variable de entorno
-DB_URL=$DATABASE_URL
-
-# Extraer las partes de la URL
-DB_HOST=$(echo $DB_URL | sed -e 's|^postgresql://\([^:]*\):.*|\1|')  # Extrae el host
-DB_PORT=$(echo $DB_URL | sed -e 's|^postgresql://.*:\([0-9]*\)/.*|\1|')  # Extrae el puerto
-DB_USER=$(echo $DB_URL | sed -e 's|^postgresql://\([^:]*\):.*|\1|')  # Extrae el usuario
-DB_NAME=$(echo $DB_URL | sed -e 's|^postgresql://.*@.*\/\(.*\)$|\1|')  # Extrae el nombre de la base de datos
+export PGPASSWORD=$DB_PASSWORD  # Se usa para la conexión de psql
 
 # Espera a que PostgreSQL esté listo para aceptar conexiones
 until psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -p "$DB_PORT" -c '\q'; do
