bbdfc334be71d04ef6194ac185dd7503c7afb1e2
Irene Gosálvez White <irenegwh@correo.ugr.es>
2025-01-23 21:20:52 +0100

    Render

--- app/container_h4_Simple/app_container/app/run.py
+++ app/container_h4_Simple/app_container/app/run.py
@@ -1,9 +1,10 @@
 from app import create_app, db
 from app.models import UserProf
+from ..logs import setup_logging
 import os 
 
 app = create_app()
-setup_logging()
+
 
 # Crear las tablas dentro del contexto de la app
 with app.app_context():
@@ -11,6 +12,7 @@ with app.app_context():
 
 # Ejecutar la app
 if __name__ == '__main__':
+    setup_logging()
     # Obtén el puerto desde la variable de entorno o usa 5000 por defecto
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
