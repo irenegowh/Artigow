9199f66c64a11651708f33cde401bc769f717cc5
Irene Gosálvez White <irenegwh@correo.ugr.es>
2025-01-23 19:55:00 +0100

    Render

--- app/container_h4_Simple/logs_container/config.py
+++ app/container_h4_Simple/logs_container/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@db_service:5432/artigow_db'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'postgresql://user:2ydBw1MGaHyvqbXYUiIWcweUsh5257SB@dpg-cu8g8elsvqrc73baddcg-a.frankfurt-postgres.render.com:5432/artigow_logs_db'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/container_h4_Simple/logs_container/wait-for-db.sh
+++ app/container_h4_Simple/logs_container/wait-for-db.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
 # Espera a que PostgreSQL esté listo para aceptar conexiones
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h db_service -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
+#until PGPASSWORD=$POSTGRES_PASSWORD psql -h db_service -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
+# Extraer los valores de la variable DATABASE_URL
+
+# Extraer los valores de las variables de entorno
+echo "DB_HOST: $DB_HOST"
+echo "DB_USER: $DB_USER"
+echo "DB_PASSWORD: $DB_PASSWORD"
+echo "DB_NAME: $DB_NAME"
+echo "DB_PORT: $DB_PORT"
+
+export PGPASSWORD=$DB_PASSWORD  # Se usa para la conexión de psql
+
+# Espera a que PostgreSQL esté listo para aceptar conexiones
+until psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -p "$DB_PORT" -c '\q'; do
   echo "Esperando a que la base de datos esté lista..."
   sleep 2
 done
 
 echo "Base de datos lista, iniciando la aplicación..."
-
-
